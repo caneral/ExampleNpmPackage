30490a9e4a59fac2051b630a188ba59566b9b4cc
Caner Al <caneral5858@gmail.com>
2022-01-26 17:49:40 +0300

    Styled-componenets added

--- src/TwCard.js
+++ src/TwCard.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './ca.css'
+import styled from 'styled-components';
 
-export const TwCard = () => {
-  return <div className='abcd'>Caner</div>;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+
+export const TwCard = ({title}) => {
+  return (
+  <Wrapper>
+    <Title>This is amazing!!</Title>
+  </Wrapper>
+  );
 };
 
 
